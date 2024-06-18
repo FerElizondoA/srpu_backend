5f622aad141ba5737cb63497e649b531e1a057dd
FerElizondoA <felizondo@cecapmex.com>
2024-06-18 15:45:05 -0600

    resolve merge

--- controllers/Reestructura.js
+++ controllers/Reestructura.js
@@ -82,4 +82,38 @@ module.exports = {
         }
       },
 
+      getRestructuras: (req, res) => {
+        const { Id, } = req.query;
+        console.log("Id: ", Id);
+     
+        if (Id == null || /^[\s]*$/.test(Id)) {
+          return res.status(409).send({
+            error: "Id de la restructura",
+          });
+        }
+     
+        db.query(
+          `CALL sp_ListadoRestructura('${Id}')`,
+          (err, result) => {
+            if (err) {
+              return res.status(500).send({
+                error: "Error",
+             
+              });
+            }
+     
+            if (result.length) {
+              const data = result[0];
+              return res.status(200).send({
+                data,
+              });
+            } else {
+              return res.status(409).send({
+                error: "¡Sin Información!",
+              });
+            }
+          }
+        );
+      },
+
 }
